bb63b3f4300c23ba55a7bfb89d087d93d8f7210a
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-03-21 20:26:11 -0300

    Integrate AWS Amplify for authentication and update signup component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import Signup from './pages/signup.jsx'  // Capital S in import
+import Signup from './pages/signup.jsx'
+
+//amplify imports
+
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
+    <Authenticator loginMechanisms={['email']}>
+    
     <div>
-      <Signup />  {/* Capital S in usage and proper spacing */}
+      
+      <Signup/>
     </div>
+    </Authenticator>
+
   )
 }
 
