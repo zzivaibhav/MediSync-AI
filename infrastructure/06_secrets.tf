bc727966ebfe61898126c6e346b60ef4c685eb96
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-03-24 12:17:34 -0300

    Add AWS Secrets Manager configuration for Medisync secrets

---
+++ infrastructure/06_secrets.tf
@@ -0,0 +1,17 @@
+resource "aws_secretsmanager_secret" "medisync_secrets" {
+  name = "medisync_secrets"
+}
+
+variable "secrets" {
+  default = {}
+}
+
+# Create a secret version with the actual values
+resource "aws_secretsmanager_secret_version" "medisync_secret_version" {
+  secret_id = aws_secretsmanager_secret.medisync_secrets.id
+  secret_string = jsonencode({
+    COGNITO_CLIENT_ID = aws_cognito_user_pool_client.client.id
+    COGNITO_CLIENT_SECRET = aws_cognito_user_pool_client.client.client_secret
+    COGNITO_USER_POOL_ID = aws_cognito_user_pool.doctor_registery.id
+  })
+}
