8893abecc2acc5e0dc2ee7781a720455cf154b99
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-04-18 04:00:16 -0300

    change

--- infrastructure/02_security_groups.tf
+++ infrastructure/02_security_groups.tf
@@ -147,12 +147,7 @@ resource "aws_security_group" "backend-load_balancer" {
     #     protocol    = "tcp"
     #     cidr_blocks = ["0.0.0.0/0"]
     # }
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+ 
     
     ingress {
         from_port   = 80
